362a83ac74db7841be591c6d9996e67f590ca4f2
qian <caiqianlucy@gmail.com>
2018-10-20 20:05:28 -0700

    MyReads Project

---
+++ src/Provider/index.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+export const MyContext=React.createContext();
+
+export default class index extends Component{
+  constructor(){
+    super();
+    this.state={
+      books:[],
+      currentlyReading:[],
+      wantToRead:[],
+      read:[],
+      addBooks: books=>{
+        const currentlyReading=books.filter(book=>book.shelf==='currentlyReading');
+        const read=books.filter(book=>book.shelf==='read');
+        const wantToRead=books.filter(book=>book.shelf==='wantToRead');
+        this.setState({books, currentlyReading, wantToRead, read});
+      },
+      moveBook:(book, newShelf, allShelves)=>{
+              const newBooks=this.state.books.map(allBooks=>{
+                const matchID=allShelves[newShelf].find(bookID=>
+                bookID===allBooks.id);
+                if(matchID){
+                  allBooks.shelf=newShelf;
+                }
+                return allBooks;
+              });
+              this.state.addBooks(newBooks);
+      }
+      }
+    }
+
+ render(){
+   return (
+     <MyContext.Provider value={this.state}>
+       {this.props.children}
+     </MyContext.Provider>
+   )
+ }
+
+}
---
+++ src/component/Fab.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+export default class Fab extends Component {
+  render(){
+    return   (
+    <div className="open-search">
+      <Link to={'/search'}> Add a book </Link>
+    </div>
+)
+  }
+}
